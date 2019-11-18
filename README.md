01994273197852c2e1bb8abfea37cc9db73f7098
Vlad Kozak <vlad.gem.typ@gmail.com>
2019-11-18 11:23:22 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# magento-cmd-export
+# Magento CMD (command line interface) Export
+Magento command line export
+
+Add script to shell/ directory in your magento project.
+
+Give your ```profile ID``` as an argument to the script (System > Import/Export > Profiles get profile ID from).
+
+Run as 
+
+```
+php shell/export.php --profile 1
+```
+
+After that, you can find file in var/export/
+
+## Trick and Treat
+1. If you get error like this `PHP Fatal error:  Allowed memory size of...`:
+
+ a. Magento parse `.httaccess` in project root before run any CLI command. 
+ This is not obvious but you need to comment next two line in that file
+ ```php
+ ...
+ ## adjust memory limit
+ 
+ #php_value memory_limit 256M
+ #php_value max_execution_time 18000
+ ...
+ ```
+ Uncomment those two line after script execution.
+ 
+ > If you want make sure in this information, please, check method `Mage_Shell_Abstract::_applyPhpVariables` 
+ 
+  
+ b. Run command as
+```
+php -d memory_limit=-1 shell/export.php --profile 1
+```
---
+++ shell/export.php
@@ -0,0 +1,112 @@
+<?php
+/**
+ * Import/Export from Shell
+ *
+ * @category Popov
+ * @package Popov_Shell
+ * @author Serhii Popov <popow.serhii@gmail.com>
+ * @datetime: 10.04.14 12:20
+ * @link http://blog.variux.com/?p=124
+ * @link http://magento.stackexchange.com/a/45671
+ * @link http://phpmysqltalk.com/1718-magento-dataflow-exportimport-form-the-command-line.html
+ */
+/**
+ * Import/Export Script to run Import/Export profile
+ * from command line or cron. Cleans entries from dataflow_batch_(import|export) table
+ */
+
+$rootPath = realpath($file = './');
+require_once $rootPath . '/shell/abstract.php';
+
+//require_once 'abstract.php';
+
+class Mage_Shell_Export extends Mage_Shell_Abstract {
+
+    /** @var  Import/Export log file */
+    protected $logFile;
+
+    public function _construct() {
+        $this->logFile = Mage::getBaseDir() . '/var/log/export_data.log';
+
+        return parent::_construct();
+    }
+
+    public function run() {
+        /** Magento Import/Export Profiles */
+        $profileId = $this->getArg('profile');
+        if ($profileId) {
+            $this->log('Starting...');
+
+            #Mage::app()->setCurrentStore(Mage_Core_Model_App::ADMIN_STORE_ID);
+
+            /** @var Mage_Dataflow_Model_Profile $profile */
+            $profile = Mage::getModel('dataflow/profile');
+
+            #$userModel = Mage::getModel('admin/user');
+            #$userModel->setUserId(0);
+            #Mage::getSingleton('admin/session')->setUser($userModel);
+
+            $profile->load($profileId);
+            if (!$profile->getId()) {
+                Mage::throwException('ERROR: Incorrect Profile for id ' . $profileId);
+            }
+
+            Mage::register('current_convert_profile', $profile);
+
+            $profile->run();
+
+            $batchModel = Mage::getSingleton('dataflow/batch');
+            $this->log('Export Complete. ProfileID: ' . $profileId . '. BatchID: ' . $batchModel->getId());
+
+            #sleep(30);
+            /** Connect to Magento database */
+            $config  = Mage::getConfig()->getResourceConnectionConfig('default_setup');
+            $db['host'] = $config->host;
+            $db['name'] = $config->dbname;
+            $db['user'] = $config->username;
+            $db['pass'] = $config->password;
+            $db['pref'] = $config->table_prefix;
+
+            /** @todo Use Magento workflow */
+            mysql_connect($db['host'], $db['user'], $db['pass']) or die(mysql_error());
+            mysql_select_db($db['name']) or die(mysql_error());
+
+            /**
+             * Truncate dataflow_batch_(import|export) table for housecleaning
+             *
+             * Post run housekeeping table bloat removal
+             * imports use "dataflow_batch_import" table
+             * exports use "dataflow_batch_export" table
+             */
+            $table = 'dataflow_batch_export';
+            $queryString = 'TRUNCATE ' . $db['pref'] . $table;
+
+            mysql_query($queryString) or die(mysql_error());
+
+            $this->log('Completed!');
+        } else {
+            echo $this->usageHelp();
+        }
+    }
+
+    protected function log($msg) {
+        echo $msg . "\n";
+        Mage::log($msg, null, $this->logFile);
+    }
+
+    /**
+     * Retrieve Usage Help Message
+     */
+    public function usageHelp()	{
+        return <<<USAGE
+Usage:  php -f export.php -- [options]
+
+  --profile <identifier>            Profile ID from System > Import/Export > Profiles
+
+USAGE;
+    }
+
+}
+
+$shell = new Mage_Shell_Export();
+$shell->run();
